c40d01c04ca1800a88b2381f6ab977ee1a1baa05
Quad15 <237855@shakopeeschools.org>
2025-09-29 11:15:50 -0500

    Update 119_Stebbins.py
    changed color of things and added some code for tommorow

--- 1.1.9 - Algorithms and Art/119_Stebbins.py
+++ 1.1.9 - Algorithms and Art/119_Stebbins.py
@@ -1,22 +1,23 @@
 import turtle as trtl
-
+#name
 pizza=trtl
+#(for later)pizza.addshape("sauce",(()))
 
 #color of crust
-pizza.color("wheat")
+pizza.color("sandybrown")
 
 #size of pizza/painter
 pizza.pensize(50)
 
 #draw pizza shape
-pizza.fillcolor("tomato")
+pizza.fillcolor("gold")
 pizza.penup()
 pizza.goto(0,-250)
 pizza.pendown()
 pizza.begin_fill()
 pizza.circle(275)
 pizza.end_fill()
-
+pizza.penup
 
 
 
