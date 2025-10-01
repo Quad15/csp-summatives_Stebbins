7ea78d1abb55b0697cafa612f5821519be2ab63f
Quad15 <237855@shakopeeschools.org>
2025-10-01 11:16:26 -0500

    Update 119_Stebbins.py
    added the pepperoni and the mushrooms made som other random stuff.

--- 1.1.9 - Algorithms and Art/119_Stebbins.py
+++ 1.1.9 - Algorithms and Art/119_Stebbins.py
@@ -1,24 +1,70 @@
 import turtle as trtl
+import random as rand
 #name
 pizza=trtl
-#(for later)pizza.addshape("sauce",(()))
-
+mush=trtl.Turtle(shape='turtle')
+mush.color("olive")
+pep=trtl.Turtle(shape='circle')
+pep.color("firebrick")
 #color of crust
 pizza.color("sandybrown")
-
+pizza.fillcolor("gold")
 #size of pizza/painter
 pizza.pensize(50)
 
-#draw pizza shape
-pizza.fillcolor("gold")
+#draw pizza shape  
 pizza.penup()
 pizza.goto(0,-250)
 pizza.pendown()
 pizza.begin_fill()
 pizza.circle(275)
 pizza.end_fill()
-pizza.penup
+pizza.penup()
 
+#draw pepperoni
+pep_q=trtl.textinput('pep','do you want pepperoni')
+if pep_q == 'y': 
+    pep_q2=trtl.textinput('pep','how much pepperoni do you want (little bit, normal, alot)')
+    if pep_q2 == 'alot':
+        for r in range(20):
+            randx=rand.randint(-137,137)
+            randy=rand.randint(-200,200)
+            pep.goto(randx,randy)
+            pep.stamp()
+    if pep_q2 == 'normal':
+        for r in range(10):
+            randx=rand.randint(-137,137)
+            randy=rand.randint(-200,200)
+            pep.goto(randx,randy)
+            pep.stamp()
+    if pep_q2 == 'little bit':
+        for r in range(5):
+            randx=rand.randint(-137,137)
+            randy=rand.randint(-200,200)
+            pep.goto(randx,randy)
+            pep.stamp()
+mush_q=trtl.textinput('mush','do you want turtles')
+if mush_q == 'y': 
+    mush_q2=trtl.textinput('pep','how many turtles do you want (little bit, normal, alot)')
+    if mush_q2 == 'alot':
+        for i in range(20):
+            randx=rand.randint(-137,137)
+            randy=rand.randint(-200,200)
+            mush.goto(randx,randy)
+            mush.stamp()
+    if mush_q2 == 'normal':
+        for i in range(10):
+            randx=rand.randint(-137,137)
+            randy=rand.randint(-200,200)
+            mush.goto(randx,randy)
+            mush.stamp()
+    if mush_q2 == 'little bit':
+        for i in range(5):
+            randx=rand.randint(-137,137)
+            randy=rand.randint(-200,200)
+            mush.goto(randx,randy)
+            mush.stamp()
+ 
 
 
 '''
