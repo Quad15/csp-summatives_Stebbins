95a0b9ade00df97037f7e7b4be95a207f7c64b24
Quad15 <237855@shakopeeschools.org>
2025-10-27 10:21:32 -0500

    Create 125_stebbins.py
    added some planning and decopmposition

---
+++ 1.2.5 - Shall We Play a Game/125_stebbins.py
@@ -0,0 +1,16 @@
+import turtle as trtl 
+import random as rand
+
+# todo create hippo and poachers 
+# create all the functions like the hippo and poachers shooting
+# ask for names for leaderboard and setup leaderboard
+
+
+
+
+
+
+
+
+wn = trtl.Screen()
+wn.mainloop()
